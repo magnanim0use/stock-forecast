dc4faa68f7b737f0f973d4e1cf5f85df266a4959
mtcrushmore <ryan@ryans-MBP-2.lan>
2023-09-08 16:04:08 -0400

    update comments

--- src/prediction/index.ts
+++ src/prediction/index.ts
@@ -4,10 +4,15 @@ import { getWeatherForecast, getMatchingWeatherDates } from '../weather';
 export async function getPrediction(stock: string, location?: string) {
     const currentStockValue = await getCurrentStockValue(stock)
     const weatherForecast = await getWeatherForecast(location || 'NYC')
+
+    /**
+        For now, find dates that match current days avg. temp.
+        TODO: predict the stock's value for the next 5+ days based on weather forecast 
+    */
     const matchingWeatherDates = await getMatchingWeatherDates(weatherForecast[0].avgTemp)
     const historicalStockValues = await getHistoricalStockValues(stock, matchingWeatherDates)
 
-    /* Find average daily change of stock at particular dates */
+    /* Find average daily change of stock at particular dates with same weather patterns. */
     const averageChange = historicalStockValues
         .map(value => (value.close - value.open) / value.open)
         .reduce((a, b) => (a + b) / historicalStockValues.length)
